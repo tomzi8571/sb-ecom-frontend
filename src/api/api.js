39570ba128cd77d0cf90fef5f85ee7fb4f6a903f
tomzi8571 <tomzi8571@users.noreply.github.com>
2025-09-24 16:14:51 +0000

    deploy: 5c276bcf471491d27c64bef3aae6773af5c0b4c7

--- src/api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`
-})
-
-export default api;
--- src/components/Filter.jsx
+++
@@ -1,5 +0,0 @@
-export const Filter = () => {
-    return (
-        <></>
-    )
-}
