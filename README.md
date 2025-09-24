9a18406c224c190908e7c45a07ec341e667032e8
Thomas Zillinger <Thomas.Zillinger@gmx.at>
2025-09-24 17:01:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Using
+
+- React
+- vite
+- tailwindCSS
+- react-router
+- react-icons 
+- react-hot-toast -> toast messages
+- headlessUI -> modal
+- material-ui -> modal
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Products} from "./components/Products.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Products/>
+        </>
+    )
+}
+
+export default App
