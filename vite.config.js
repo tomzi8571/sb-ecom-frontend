15870d769c181af74dc8b8672e100e70efd9d42a
Thomas Zillinger <Thomas.Zillinger@gmx.at>
2025-09-24 17:42:41 +0200

    feat: - add deployment to github pages after commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/<your-repo-name>/',
   plugins: [
       react(),
       tailwindcss()  ],
