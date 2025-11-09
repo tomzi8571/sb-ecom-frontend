aa9c2ea778b2bba4e919ada4bdfa3f898b158e61
Thomas Zillinger <Thomas.Zillinger@gmx.at>
2025-11-09 01:04:19 +0100

    feat: - fix env

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
+console.log("Frontend Url: ", import.meta.env.VITE_FRONTEND_URL)
+console.log("Base Url: ", import.meta.env.VITE_FRONTEND_BASE_URL)
+console.log("Backend Url: ", import.meta.env.VITE_BACK_END_URL)
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: import.meta.env.VITE_FRONTEND_BASE_URL || './',
   plugins: [
       react(),
       tailwindcss()  ],
