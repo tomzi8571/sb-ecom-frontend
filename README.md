6d705efcf300ff671961aa65110bd86e2fff564b
Thomas Zillinger <Thomas.Zillinger@gmx.at>
2025-11-11 22:50:36 +0100

    feat: - test 404 redirect for github pages

--- README.md
+++ README.md
@@ -6,13 +6,30 @@
 - react-router
 - react-icons 
 - react-hot-toast -> toast messages
-- headlessUI -> modal
 - material-ui : https://mui.com/material-ui/
-- react-loader-spinners -> loading
-- swiper -> HeroBanner
-- Stepper (Material UI) -> Checkout
-- Dialog Modal -> headless UI Dialog
+  - Skeleton: https://mui.com/material-ui/react-skeleton/
+  - Stepper: https://mui.com/material-ui/react-stepper/
+  - Dialog: https://mui.com/material-ui/react-dialog/
+  - 
+- Skeletons:
+  - https://skeletonreact.com/
+  - https://www.npmjs.com/package/react-loading-skeleton
+  - https://mui.com/material-ui/react-skeleton/
+- Spinner
+  - https://www.npmjs.com/package/react-loader-spinner
+  - https://www.npmjs.com/package/react-spinners
+  - https://www.npmjs.com/package/react-spinners-kit
+- Swiper
+  - https://swiperjs.com/react  (Hero Banner)
+- Dialog Modal
+  - https://headlessui.com/react/dialog
+- Placeholder Images
+  - https://placehold.co/
+
+
 
 # Images 
 Freely available images from pexels.com
+- https://picsum.photos/
+- https://pexels.com
 Contacts: https://www.pexels.com/photo/woman-writing-on-a-notebook-beside-teacup-and-tablet-computer-733856/
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,37 @@
 /* eslint-env node */
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 // IMPORTANT: this config runs in Node (during dev/build). Do NOT use import.meta.env here.
 // Use loadEnv(mode, process.cwd(), '') to read VITE_ variables for the current mode.
 
-export default defineConfig(({ mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(({mode}) => {
+    // eslint-disable-next-line no-undef
+    const env = loadEnv(mode, process.cwd(), '')
 
-  // Preferably keep variable names consistent in your .env files. Support a common alternate key for backend URL.
-  const frontendUrl = env.VITE_FRONTEND_URL || ''
-  const baseUrl = env.VITE_FRONTEND_BASE_URL || './'
-  const backendUrl = env.VITE_BACK_END_URL || ''
+    // Preferably keep variable names consistent in your .env files. Support a common alternate key for backend URL.
+    const frontendUrl = env.VITE_FRONTEND_URL || ''
+    const baseUrl = env.VITE_FRONTEND_BASE_URL || './'
+    const backendUrl = env.VITE_BACK_END_URL || ''
 
-  console.log('Using Vite env (mode=', mode, ')')
-  console.log('VITE_FRONTEND_URL =', frontendUrl)
-  console.log('VITE_FRONTEND_BASE_URL =', baseUrl)
-  console.log('VITE_BACK_END_URL =', backendUrl)
+    // prefer VITE_PORT, default 5173
+    const port = Number(env.VITE_PORT || 5173)
 
-  return {
-    base: baseUrl,
-    plugins: [
-      react(),
-      tailwindcss()
-    ]
-  }
+    console.log('Using Vite env (mode=', mode, ')')
+    console.log('VITE_FRONTEND_URL =', frontendUrl)
+    console.log('VITE_FRONTEND_BASE_URL =', baseUrl)
+    console.log('VITE_BACK_END_URL =', backendUrl)
+    console.log('VITE_PORT =', port)
+
+    return {
+        base: baseUrl,
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
+        server: {
+            port
+        }
+    }
 })
